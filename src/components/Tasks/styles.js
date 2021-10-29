56ff34a09fc95f03db2292f0a37097f801eb147a
Paulo Xavier <pxavierf@gmail.com>
2021-10-29 15:04:03 -0300

    estilizando o container do tasks

--- src/components/Tasks/styles.js
+++ src/components/Tasks/styles.js
@@ -1,5 +1,12 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  box-shadow: 0 0 15px -4px rgba(0,0,0,0.75);
+  border-top: 3px solid ${({ theme }) => theme.primary};;
+  border-radius: 0.25rem;
+  background: ${({ theme }) => theme.backgroundLight};
+  margin: 2rem;
 
+  max-width: 1020px;
+  width: 100%;
 `;
