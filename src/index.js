573bee51f9dfd54c864122f60ee0ec4fefcf101f
Paulo Xavier <pxavierf@gmail.com>
2021-10-29 12:53:27 -0300

    configurando banco e schema no mirage

--- src/index.js
+++ src/index.js
@@ -1,14 +1,61 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createServer, Model } from 'miragejs';
 import { ThemeProvider } from 'styled-components';
 import App from './App';
 import theme from '../src/styles/theme';
-import GlobalStyles from '../src/styles/global'
-import reportWebVitals from './reportWebVitals';
+import GlobalStyles from '../src/styles/global';
+
+createServer({
+  models: {
+    task: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      tasks: [
+        {
+          _id: 1,
+          task: 'Primeira tarefa cadastrada',
+          status: 'pendente',
+          priority: 'baixa',
+          createAt: new Date(),
+        },
+        {
+          _id: 2,
+          task: 'Segunda tarefa cadastrada',
+          status: 'pronto',
+          priority: 'media',
+          createAt: new Date(),
+        },
+        {
+          _id: 3,
+          task: 'Terceira tarefa cadastrada',
+          status: 'em andamento',
+          priority: 'alta',
+          createAt: new Date(),
+        }
+      ],
+    });
+  },
+
+  routes() {
+    this.namespace = 'api';
+
+    this.get('tasks', () => {
+      return this.schema.all('task');
+    });
+
+    this.post('tasks', (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+
+      return schema.create('task', { ...data, createAt: new Date() })
+    });
+  }
+});
 
 ReactDOM.render(
   <React.StrictMode>
-
     <ThemeProvider theme={ theme }>
       <App />
       <GlobalStyles />
@@ -16,8 +63,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
